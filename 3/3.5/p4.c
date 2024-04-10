7c43d179d2d2cb6cd0250c2f2f0b3839127b3b0b
hisava <veretennikovalexey@gmail.com>
2024-04-10 12:15:11 +0300

    alex

--- 3/3.5/p4.c
+++ 3/3.5/p4.c
@@ -2,11 +2,15 @@
 
 int main(void)
 {
-    unsigned char b, not_b;
-    scanf("%hhu", &b);
+    unsigned char bits;
+    scanf("%hhu", &bits);
 
-    not_b = ~b;
-    printf("%d\n", not_b);    
-    
+    unsigned char mask = 0b00101000; // 40
+
+    if ((bits & mask) == mask)
+        printf( "%d\n", bits);
+    else
+        printf( "%d\n", -1 );
+
     return 0;
 }
