43b1d824f643e58e2a3fc3abfa0e264e5a68622c
hisava <veretennikovalexey@gmail.com>
2024-04-19 08:09:05 +0300

    alex

--- 3/3.9/p4.c
+++ 3/3.9/p4.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 
-const int max_size = 5;
-
 #define SIZE    1
 #define IS_CPP
 
-#if max_size > 1 
-printf( "hello" );
-#else
+int main( void ) {
 
-#endif
+    const int max_size = 5;
+
+#ifdef(IS_CPP)
+#endif    
+
+
+    return 0;
+}    
+
+// https://proproprogs.ru/c_base/c_direktivy-include-i-uslovnoy-kompilyacii
+
+// #if SIZE > 0 ... #endif
+// #ifdef(IS_CPP) ... #endif При использовании #ifdef не следует заключать следующий идентификатор в скобки
