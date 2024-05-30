e995474825849a73f794f3c4bbe0a96d163f97dc
hisava <veretennikovalexey@gmail.com>
2024-05-30 16:52:43 +0300

    .

---
+++ 9/9.5/p3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main() {
+    char str[ 50 ];
+    cin >> str;
+    cout << str << endl;
+
+    return 0;
+}
+
+//     std::cout << "" << std::endl;
